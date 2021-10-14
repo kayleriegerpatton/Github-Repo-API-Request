62959e9a409780007bb075a41a6476f64045d041
Kayle Rieger Patton <kayle.patton22@gmail.com>
2021-10-14 23:14:57 +0100

    cards rendered with API data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,39 +1,44 @@
 const mainContainer = $("#main-container");
 
-const handleResponse = function () {};
+// Where/how did this function use 'repos' when it's not defined anywhere in the code?
+const renderCards = function (repos) {
+  const constructCards = function (eachRepo) {
+    //   get repo name
+    const repoName = eachRepo.name;
+    console.log(repoName);
 
-const renderCards = function () {
-  //   get repo name
-  const repoName = repos.name;
+    //   get link
+    const repoLink = eachRepo.html_url;
 
-  //   get link
-  const repoLink = repos.html_url;
-
-  // construct card
-  const repoCard = `<div class="card" style="width: 18rem">
-  <img src="..." class="card-img-top" alt="..." />
+    // construct card
+    const repoCard = `<div class="card" style="width: 18rem">
+  <img src="" class="card-img-top" alt="" />
   <div class="card-body">
     <h5 class="card-title">${repoName}</h5>
     <p class="card-text"></p>
     <a href="${repoLink}" class="btn btn-primary">Deployed Application</a>
   </div>
 </div>`;
-
+    // append to main container
+    mainContainer.append(repoCard);
+  };
   // map over repo data
+  repos.forEach(constructCards);
 };
 
-const handleError = function () {};
+const onLoad = function () {
+  const handleResponse = function (dataResponse) {
+    // *** What is this doing? I don't understand the console logged info either.
+    return dataResponse.json();
+  };
 
-// get data from API
-const getAPIData = function () {
+  // const handleError = function () {};
+
+  // get data from API
   fetch("https://api.github.com/users/kayleriegerpatton/repos")
     .then(handleResponse)
-    .then(renderCards)
-    .catch(handleError);
+    .then(renderCards);
+  // .catch(handleError);
 };
 
-fetch("https://api.github.com/users/kayleriegerpatton/repos").then(function (
-  response
-) {
-  console.log();
-});
+window.addEventListener("load", onLoad);
